1d7507436e83cc8aedcea515b1dfcc27d238b56f
vovasemkovych <37297861+vovasemkovych@users.noreply.github.com>
2021-05-21 09:50:58 +0300

    Add files via upload

---
+++ setup_viewer.js
@@ -0,0 +1,60 @@
+/* global webots: false */
+
+var view = null;
+var ipInput = null;
+var portInput = null;
+var connectButton = null;
+var modeSelect = null;
+var broadcast = null;
+
+var mobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+if (mobileDevice) {
+  let head = document.getElementsByTagName('head')[0];
+  let jqueryTouch = document.createElement('script');
+  jqueryTouch.setAttribute('type', 'text/javascript');
+  jqueryTouch.setAttribute('src', 'https://www.cyberbotics.com/jquery-ui/1.11.4/jquery.ui.touch-punch.min.js');
+  head.appendChild(jqueryTouch);
+
+  var mobileCss = document.createElement('link');
+  mobileCss.setAttribute('rel', 'stylesheet');
+  mobileCss.setAttribute('type', 'text/css');
+  mobileCss.setAttribute('href', 'https://www.cyberbotics.com/wwi/R2021a/wwi_mobile.css');
+  head.appendChild(mobileCss);
+}
+
+function init() {
+  ipInput = document.getElementById('IPInput');
+  connectButton = document.getElementById('ConnectButton');
+  modeSelect = document.getElementById('mode');
+  broadcast = document.getElementById('broadcast')
+}
+
+function connect() {
+
+  let playerDiv = document.getElementById('playerDiv');
+  view = new webots.View(playerDiv, mobileDevice);
+  view.broadcast = broadcast.checked;
+  view.setTimeout(-1); // disable timeout that stops the simulation after a given time
+  const streamingMode = modeSelect.options[modeSelect.selectedIndex].value;
+  view.open(ipInput.value, streamingMode);
+  view.onquit = disconnect;
+  connectButton.value = 'Disconnect';
+  connectButton.onclick = disconnect;
+  ipInput.disabled = true;
+  modeSelect.disabled = true;
+  broadcast.disabled = true;
+}
+
+function disconnect() {
+  view.close();
+  view = null;
+  let playerDiv = document.getElementById('playerDiv');
+  playerDiv.innerHTML = null;
+  connectButton.value = 'Connect';
+  connectButton.onclick = connect;
+  ipInput.disabled = false;
+  modeSelect.disabled = false;
+  broadcast.disabled = false;
+}
+
+window.addEventListener('load', init, false);
